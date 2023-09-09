1f0502d19df0145ac2a95e4e1e87436a6ed4fedd
User <pavadi.abhishek@gmail.com>
2023-09-09 04:15:55 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { EmployeeModel } from './model/employee.model';
+import { EmployeeService } from './services/empservice';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  title = 'SpringBootEmploye_Integration1';
+
+  firstName:String;
+  lastName:String;
+  email:String;
+  
+  editMode:boolean=false;
+  currentEmployeeId:string;
+  
+  @ViewChild('empForm') form:NgForm ;
+   allemployees:EmployeeModel[]=[];
+ constructor(private empService:EmployeeService){}
+
+ //-----------------------SAVE Employe with type EmpModel-----------------//
+   saveEmployee(emp: EmployeeModel)
+  {
+console.log(emp);
+if(!this.editMode)
+this.empService.saveEmployee(emp);
+else
+this.empService.updateEmployee(this.currentEmployeeId,emp);
+this.form.setValue({
+  firstName:'',
+  lastName:'',
+  email:''
+})
+  }
+
+  //--------------------FETCH ALL EMPLOYEES-------------------//
+
+  getAllEmployees() {
+    alert("from app component--");
+    this.empService.fetchEmployees()
+    .subscribe((employees)=>{
+       this.allemployees=employees;
+    })
+  
+  }
+
+  deleteEmployee(id:string)
+  {
+    this.empService.deleteEmployee(id).subscribe((res)=>{
+               console.log(res);
+    })
+  }
+
+  //----------------------------EDIT EMPLOYEE-----------------//
+  onEditEmployee(id:string)
+  {
+   //Ge the current employee id
+   let currentEmployee=this.allemployees.find((p)=>{return p.id===id});
+   console.log(currentEmployee);
+
+   //populate the form with employe id
+   this.form.setValue(
+    {
+      firstName:currentEmployee.firstName,
+      lastName:currentEmployee.lastName,
+      email:currentEmployee.email,
+    }
+   )
+   this.editMode=true;
+  }
+
+  deleteAllEmplpoyees()
+  {
+  this.empService.deleteAllEmplpoyees();
+  }
+  
+}
---
+++ src/app/services/empservice.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from "@angular/common/http";
+import { EmployeeModel } from "../model/employee.model";
+import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EmployeeService
+{
+    constructor(private http:HttpClient){}
+    
+    //-------------------Save Employee-------------------//
+    public save()
+    {
+        alert("save");
+    }
+    saveEmployee(emp:EmployeeModel)
+    {
+        return this.http.post('http://localhost:8080/api/employees/save',emp,{responseType: 'text'}).subscribe((response)=>{console.log("EmployeeSaved") });
+       alert("save method called");
+    }
+
+    public fetchEmployees()
+    {
+return this.http.get<{[key:string]:EmployeeModel}>('http://localhost:8080/api/employees/getAll')
+                 .pipe(map((res)=>{
+                    const employees=[];
+                    for(const key in res)
+                    {
+                        if(res.hasOwnProperty(key))
+                        {
+                            employees.push({...res[key]})
+                        }
+                       
+                    }
+                    return employees;
+                 }))
+    }
+
+    deleteEmployee(id:string)
+    {
+        return this.http.delete('http://localhost:8080/api/employees/delete/'+id,{responseType:'text'});
+    }
+
+    updateEmployee(id:string,value:EmployeeModel)
+    {
+        alert("update employee" +id);
+        this.http.put('http://localhost:8080/api/employees/'+id,value).subscribe();
+    }
+
+    deleteAllEmplpoyees()
+    {
+        this.http.delete('http://localhost:8080/api/employees/deleteAll').subscribe();
+    }
+}
